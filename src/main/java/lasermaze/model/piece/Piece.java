5a69cc5840bbc0c37faeb3992123b003020ad971
Brad Lee <brad903@naver.com>
2019-02-13 15:28:28 +0900

    [19-02-13] Piece에 User 추가

--- src/main/java/lasermaze/model/piece/Piece.java
+++ src/main/java/lasermaze/model/piece/Piece.java
@@ -4,13 +4,16 @@ import lasermaze.model.Board;
 import lasermaze.model.Direction;
 import lasermaze.model.Point;
 import lasermaze.model.Rotation;
+import lasermaze.model.user.User;
 
 public abstract class Piece implements Pieceable {
     protected Point point;
     protected Direction direction;
+    private User user;
     private Playable playable;
 
-    public Piece(Playable playable) {
+    public Piece(User user, Playable playable) {
+        this.user = user;
         this.playable = playable;
     }
 
